9a6f2b1d98135250c82cf7a729aa3bd7ee383ceb
Meche221 <binaymeche22@gmail.com>
2025-08-12 12:25:50 +0545

    bug fixed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input){
-    display.value += input
+    if(display.value == "Error"){
+        display.value = ""
+    }
+    display.value += input;
 }
 
 function clearDisplay(){
